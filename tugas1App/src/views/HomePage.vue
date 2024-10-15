6757e9d05c81c00803be20b8c671c400e8abec23
engineerlocalhost <engineerlocalhost@gmail.com>
2024-10-15 22:12:07 +0700

    v

--- tugas1App/src/views/HomePage.vue
+++ tugas1App/src/views/HomePage.vue
@@ -1,9 +1,25 @@
 <style scoped>
   .example-content {
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
     height: 100%;
+    text-align: center;
+  }
+  .profile-image {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    margin-bottom: 16px;
+  }
+  .profile-name {
+    font-size: 24px;
+    font-weight: bold;
+  }
+  .profile-description {
+    font-size: 16px;
+    color: gray;
   }
 </style>
 
@@ -11,11 +27,15 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Listen now</ion-title>
+        <ion-title>Home</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content>
-      <div class="example-content">Listen now content</div>
+      <div class="#">
+        <img src="#" alt="Profile Picture" class="profile-image" />
+        <div class="#">Nama Anda</div>
+        <div class="#">Deskripsi singkat tentang diri Anda.</div>
+      </div>
     </ion-content>
   </ion-page>
 </template>
